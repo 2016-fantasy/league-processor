74388d99a3010fbba19212621ff23642f921b214
Blake La Pierre <blakelapierre@gmail.com>
2015-09-28 20:59:28 -0700

    Removing what seems like unneccessary cruft...we'll see

--- src/core.js
+++ src/core.js
@@ -1,3 +1,7 @@
+import {g} from 'generator-trees';
+
+const {map} = g;
+
 export default (handlers, eventGenerator, update) => {
   let clock = 0;
 
@@ -9,21 +13,7 @@ export default (handlers, eventGenerator, update) => {
 
     outgoing.$clock = clock;
 
-    do {
-      let {value, done} = events.next(),
-          result = process(value);
-
-      console.log({result});
-      if (result) {
-        outgoing.push(result); // this could be more complicated...returns null, multiple results?
-        yield result;
-      }
-
-      //something should be different here?
-      // yield result;
-
-      if (done) break;
-    } while (true);
+    yield* map(events, process);
 
     update(clock);
 
@@ -34,69 +24,9 @@ export default (handlers, eventGenerator, update) => {
     console.log('Processing', {event});
 
     return (handlers[event.type] || defaultHandler)(event, clock);
-
-    // while(let {value, done} = events.next()) {
-    //   const result = (handlers[type] || defaultHandler)(value);
-
-    //   if (done) return result;
-    //   else yield result;
-    // }
   }
 
   function defaultHandler() {
     console.log('Default!');
   }
-
-  // function tick(transport) {
-  //   clock++;
-
-  //   transport.clock = clock;
-  //   transport.events = processEventQ();
-
-  //   update(clock);
-
-  //   transport.output = swapQ();
-
-  //   return transport;
-  // }
-
-  // function processEventQ() {
-  //   let events = swapQ(getEventsFn());
-  // }
-
-  // function swapQ(newQ) {
-  //   let events = newQ || eventQ;
-  //   eventQ = [];
-  //   return events;
-  // }
 };
-
-// export default (handlers, eventGenerator, update) => {
-//   let clock = 0,
-//       eventQ = [];
-
-//   return tick;
-
-//   function tick(transport) {
-//     clock++;
-
-//     transport.clock = clock;
-//     transport.events = processEventQ();
-
-//     update(clock);
-
-//     transport.output = swapQ();
-
-//     return transport;
-//   }
-
-//   function processEventQ() {
-//     let events = swapQ(getEventsFn());
-//   }
-
-//   function swapQ(newQ) {
-//     let events = newQ || eventQ;
-//     eventQ = [];
-//     return events;
-//   }
-// };
